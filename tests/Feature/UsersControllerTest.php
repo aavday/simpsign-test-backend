1f477ad62c4a69563d17a57824e447560803e90d
aavday <wrck7@yandex.ru>
2024-09-15 18:18:43 +0300

    Create test for checking that users descripton not empty

---
+++ tests/Feature/UsersControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Tests\TestCase;
+
+class UsersControllerTest extends TestCase
+{
+    public function test_that_description_array_not_empty(): void
+    {
+        $users = (new UsersController)->index();
+        $isPassed = true;
+
+        $users->contains(function ($user) use (&$isPassed) {
+           if (!Json::decode($user->description)) {
+               $isPassed = false;
+           }
+        });
+
+        $this->assertTrue($isPassed);
+    }
+}
